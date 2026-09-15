539feb1d5200b502a2d709a2f90d5b43d670de75
Esta Golemi <145614659+estagolemi@users.noreply.github.com>
2026-09-16 00:04:20 +0200

    Update email sending logic and add quote feature
    Refactor birthday email functionality and add weekly quote feature.

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
-import pandas
-import random
+import datetime as dt
 import smtplib
+import random
 import os
 
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+now = dt.datetime.now()
+day_of_week = now.weekday()
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+if day_of_week == 3:
+    with open("quotes.txt", "r") as file:
+        quotes = file.readlines()
+        quote_of_the_week = random.choice(quotes)
+    print(quote_of_the_week)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+        connection.login(user=my_email, password=password)
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg=f"Subject: Monday Motivational Quote\n\n {quote_of_the_week}"
         )
+
+
